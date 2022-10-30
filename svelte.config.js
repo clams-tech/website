9581ae0761ca550c1971b113af739496b4819674
Aaron <abarnard@protonmail.com>
2022-10-30 12:55:10 +1100

    Again

--- svelte.config.js
+++ svelte.config.js
@@ -10,26 +10,6 @@ const config = {
 		adapter: adapter(),
 		paths: {
 			base: production ? '/website' : ''
-		},
-		csp: {
-			mode: 'hash',
-			directives: {
-				'script-src': [
-					'self',
-					'unsafe-eval', // needed for lnsocket script
-					'unsafe-inline'
-				],
-				'object-src': ['self']
-			},
-			reportOnly: {
-				'script-src': ['self', 'unsafe-eval'],
-				'report-to': ['self'],
-				'report-uri': ['self'],
-				'object-src': ['self']
-			}
-		},
-		csrf: {
-			checkOrigin: true
 		}
 	}
 }
