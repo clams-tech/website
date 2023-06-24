0cb1ec4748bf1378a89c27fb9a32b52d2dadb614
johngribbin <30157175+johngribbin@users.noreply.github.com>
2023-06-24 16:59:12 +0100

    Add background images and update styles of homepage

--- src/lib/elements/Button.svelte
+++ src/lib/elements/Button.svelte
@@ -27,7 +27,7 @@
 	bind:this={button}
 	on:click
 	style={`opacity: ${disabled ? '0.4' : '1'}`}
-	class="text-current no-underline text-{small ? 'xs' : 'base'} hover:shadow-{primary
+	class="bg-black text-current no-underline text-{small ? 'xs' : 'base'} hover:shadow-{primary
 		? 'purple-500'
 		: 'current'} active:shadow-sm shadow-sm hover:shadow-md disabled:bg-disabled disabled:border-disabled w-full flex items-center justify-center rounded-md py-3 px-{small
 		? '2'
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,6 +19,13 @@
 			imgSrc: lightning
 		},
 		{
+			title: 'Charts',
+			description:
+				'Interactive charts to visualize data over time. Everything from node uptime to income events.',
+			learnHref: '/charts',
+			imgSrc: accountancy
+		},
+		{
 			title: 'Offers',
 			description: '@TODO - talk about current support',
 			learnHref: '/offers',
@@ -29,13 +36,6 @@
 			description: '@TODO - full LNURL, pay and withdraw supported.',
 			learnHref: '/lnurl',
 			imgSrc: lightning
-		},
-		{
-			title: 'Charts',
-			description:
-				'Interactive charts to visualize data over time. Everything from node uptime to income events.',
-			learnHref: '/charts',
-			imgSrc: accountancy
 		},
 		{
 			title: 'Exports',
@@ -87,8 +87,10 @@
 <!-- <h1 class="fixed">{scroll}</h1> -->
 <Links />
 <!-- Hero -->
-<section class="flex items-center justify-center w-full h-screen border">
-	<div class="flex justify-between w-full max-w-4xl">
+<section
+	class="flex items-center justify-center w-full h-screen bg-[url('$lib/images/background6.png')] bg-cover"
+>
+	<div class="flex justify-between w-full max-w-4xl bg-black/[.6] p-10 rounded-lg gap-10">
 		<div class="flex flex-col max-w-md gap-10">
 			<h1 class="text-8xl">Unified Bitcoin.</h1>
 			<!-- TODO - add helvetica to subtitle copy -->
@@ -96,7 +98,7 @@
 			<!-- @TODO - add summary of why would you use clams? privacy focused, local data, no logging, not tracking, all data stays on your device, bitcoin first, private, local -->
 			<div class="flex">
 				<a class="mr-4" href="https://app.clams.tech" target="_blank" rel="noopener noreferrer">
-					<Button text="Start Now" primary />
+					<Button text="Start now" primary />
 				</a>
 				<!-- TODO - add helvetica to button copy -->
 				<a href="https://docs.clams.tech" target="_blank" rel="noopener noreferrer">
@@ -104,27 +106,29 @@
 				</a>
 			</div>
 		</div>
-		<div class="w-full max-w-sm">
+		<div class="w-full max-w-sm invisible md:visible">
 			<ClamsLogo />
 		</div>
 	</div>
 </section>
 <!-- Features -->
-{#each features as { title, description, learnHref, imgSrc }}
-	<section class="flex items-center justify-center w-full h-screen border">
-		<div class="w-full max-w-md">
-			<div class="flex flex-col items-start gap-10">
-				<a href={learnHref}>
-					<h1 class="text-8xl">{title}</h1>
-				</a>
-				<h2 class="text-2xl">{description}</h2>
-				<a href={learnHref}>
-					<Button text="Learn more" />
-				</a>
+{#each features as { title, description, learnHref, imgSrc }, i}
+	<section class="flex items-center justify-center w-full h-screen">
+		<div class="flex justify-between w-full max-w-4xl bg-white/[.2] p-10 rounded-lg gap-10">
+			<div class="flex flex-col max-w-md gap-10">
+				<div class="flex flex-col items-start max-w-md gap-10">
+					<a href={learnHref}>
+						<h1 class="text-8xl">{title}</h1>
+					</a>
+					<h2 class="text-2xl">{description}</h2>
+					<a href={learnHref}>
+						<Button text="Learn more" />
+					</a>
+				</div>
 			</div>
-		</div>
-		<div class="w-full max-w-sm">
-			<img src={imgSrc} alt={`${title}`} />
+			<div class="w-full max-w-sm invisible md:visible">
+				<img src={imgSrc} alt={`${title}`} />
+			</div>
 		</div>
 	</section>
 {/each}
