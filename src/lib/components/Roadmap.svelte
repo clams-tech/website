edc0d34ccec31934749ce672978b73c5de630898
johngribbin <30157175+johngribbin@users.noreply.github.com>
2023-10-05 08:58:06 -0400

    Improve roadmap copy

--- src/lib/components/Roadmap.svelte
+++ src/lib/components/Roadmap.svelte
@@ -1,52 +1,53 @@
 <script lang="ts">
-	type Status = 'todo' | 'started'
+	type Status = 'planned' | 'inProgress'
 
 	const roadMap: { title: string; description: string; status: Status }[] = [
 		{
-			title: 'Metadata',
-			description: 'Add feature to tag transactions to provide accounting insights.',
-			status: 'started'
+			title: 'Transaction Tagging',
+			description: 'Giver users option to tag transactions for enhanced accounting insights.',
+			status: 'inProgress'
 		},
 		{
-			title: 'LND Support',
-			description: 'Enhance wallet feature by including LND as a connection option.',
-			status: 'started'
+			title: 'LND Integration',
+			description: 'Integrate LND lightning implementation support to improve accessibility.',
+			status: 'inProgress'
 		},
 		{
-			title: 'Charts',
-			description: 'Interactive charts feature to provide accounting insights',
-			status: 'todo'
-		},
-		{
-			title: 'Trades',
+			title: 'Interactive Charts',
 			description:
-				'Integration with exchanges to track buys and sells to inform accountancy algorithims.',
-			status: 'todo'
+				'Bring data to life with interactive charts that provide deep accounting insights.',
+			status: 'planned'
+		},
+		{
+			title: 'Exchange Integration',
+			description: 'Connect with popular exchanges to automatically track buy and sell activities.',
+			status: 'planned'
 		},
 		{
 			title: 'xPUB Wallet Support',
-			description: 'This is the metadata description',
-			status: 'todo'
+			description: 'Expand wallet feature with extended public key (xPUB) connection support.',
+			status: 'planned'
 		},
 		{
 			title: 'Historical Pricing Server',
-			description: 'Server infra to help with cost-basic calculations.',
-			status: 'todo'
+			description:
+				'Implement a dedicated server infrastructure for precise cost-basis calculations.',
+			status: 'planned'
 		},
 		{
-			title: 'Cap Gains Algorithims',
+			title: 'Capital Gains Algorithms',
 			description:
-				'Cost-basis, profit and loss etc. Will cover all activity across all connected wallets.',
-			status: 'todo'
+				'Comprehensive algorithms for calculating cost basis, profit, and loss across all connected wallets.',
+			status: 'planned'
 		}
 	]
 </script>
 
 <div class="grid md:grid-cols-2 gap-5">
 	<div class="flex flex-col gap-3">
-		<h2 class="font-bold">TODO</h2>
+		<h2 class="font-bold">Planned</h2>
 		{#each roadMap as { title, description, status }}
-			{#if status === 'todo'}
+			{#if status === 'planned'}
 				<div class="w-full">
 					<p class="font-bold">{title}</p>
 					<p>{description}</p>
@@ -55,9 +56,9 @@
 		{/each}
 	</div>
 	<div class="flex flex-col gap-3">
-		<h2 class="font-bold">STARTED</h2>
+		<h2 class="font-bold">In Progress</h2>
 		{#each roadMap as { title, description, status }}
-			{#if status === 'started'}
+			{#if status === 'inProgress'}
 				<div class="w-full">
 					<p class="font-bold">{title}</p>
 					<p>{description}</p>
