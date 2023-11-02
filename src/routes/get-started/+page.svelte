2c86f527b78237c4a2bba4aa238cff09d450a965
johngribbin <30157175+johngribbin@users.noreply.github.com>
2023-11-01 21:00:02 -0400

    copy change

--- src/routes/get-started/+page.svelte
+++ src/routes/get-started/+page.svelte
@@ -9,14 +9,14 @@
 	console.log(`data = `, data)
 </script>
 
-<section class="flex flex-col items-center px-6 pt-28 pb-20">
-	<div class="flex flex-col w-full max-w-3xl text-center gap-16">
+<section class="flex flex-col items-center px-6 pt-32 pb-20">
+	<div class="flex flex-col w-full max-w-2xl text-center gap-16">
 		<h1 class="text-6xl font-bold">Get Started</h1>
 		<!-- Desktop app -->
 		<div class="flex flex-col items-center">
 			<p class="text-[20px]">
-				For the optimal experience, we recommend using our Native Desktop App. Please select the
-				option that corresponds to your operating system:
+				For the optimal experience, we recommend downloading the Native Desktop App. Please select
+				the option that corresponds to your operating system:
 			</p>
 			<div class="mt-8 flex gap-8">
 				<a
@@ -61,8 +61,7 @@
 		<!-- PWA -->
 		<div class="flex flex-col items-center">
 			<p class="text-[20px]">
-				If you'd like to use Clams on your mobile device, you can do so through our Progressive Web
-				App:
+				If you'd like to use Clams on your mobile device, you can use the Progressive Web App:
 			</p>
 			<div class="mt-8 flex gap-6">
 				<a
