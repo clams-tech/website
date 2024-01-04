a03c1e3642f32b05d1ec82ba048c23968acb0199
johngribbin <30157175+johngribbin@users.noreply.github.com>
2024-01-04 18:58:59 -0500

    Icon updates

--- src/lib/icons/abacus.ts
+++ src/lib/icons/abacus.ts
@@ -21,23 +21,6 @@ export default `
 <ellipse cx="215" cy="195" rx="33" ry="18" fill="currentColor"/>
 <ellipse cx="215" cy="266" rx="33" ry="18" fill="currentColor"/>
 <ellipse cx="215" cy="302" rx="33" ry="18" fill="currentColor"/>
-<mask id="mask0_188_17175" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="182" y="284" width="66" height="36">
-<ellipse cx="215" cy="302" rx="33" ry="18" fill="#3F039A"/>
-</mask>
-<g mask="url(#mask0_188_17175)">
-<g filter="url(#filter0_f_188_17175)">
-<path d="M213.64 284.235L185.754 297.281C184.803 297.726 185.093 299.148 186.142 299.186L243.506 301.246C244.575 301.284 244.95 299.844 243.998 299.357L214.519 284.251C214.244 284.11 213.92 284.104 213.64 284.235Z" fill="url(#paint0_linear_188_17175)" fill-opacity="0.26"/>
-</g>
-<g style="mix-blend-mode:multiply" opacity="0.15" filter="url(#filter1_f_188_17175)">
-<path d="M240.915 302.853L184.272 298.33C183.403 298.261 182.868 299.257 183.405 299.943L193.297 312.582C193.415 312.733 193.574 312.847 193.755 312.91L211.832 319.226C212.026 319.294 212.237 319.3 212.435 319.244L231.035 313.963C231.184 313.921 231.32 313.845 231.434 313.742L241.508 304.59C242.156 304.001 241.788 302.923 240.915 302.853Z" fill="currentColor"/>
-</g>
-<g filter="url(#filter2_f_188_17175)">
-<path d="M199 317C211.708 319.195 212.563 318.995 216.966 318.995C222.225 318.995 226.633 318.167 229.236 317.998C232.304 317.798 234.057 317 232.304 317.599" stroke="currentColor"/>
-</g>
-<g filter="url(#filter3_f_188_17175)">
-<path d="M199 286C211.708 283.805 212.563 284.005 216.966 284.005C222.225 284.005 226.633 284.833 229.236 285.002C232.304 285.202 234.057 286 232.304 285.401" stroke="currentColor"/>
-</g>
-</g>
 <ellipse cx="283" cy="302" rx="33" ry="18" fill="currentColor"/>
 <ellipse cx="283" cy="266" rx="33" ry="18" fill="currentColor"/>
 <ellipse cx="283" cy="231" rx="33" ry="18" fill="currentColor"/>
@@ -46,31 +29,5 @@ export default `
 <ellipse cx="78" cy="231" rx="33" ry="18" fill="currentColor"/>
 <ellipse cx="78" cy="266" rx="33" ry="18" fill="currentColor"/>
 <ellipse cx="78" cy="302" rx="33" ry="18" fill="currentColor"/>
-<defs>
-<filter id="filter0_f_188_17175" x="180.177" y="279.141" width="69.3672" height="27.106" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-<feGaussianBlur stdDeviation="2.5" result="effect1_foregroundBlur_188_17175"/>
-</filter>
-<filter id="filter1_f_188_17175" x="178.189" y="293.327" width="68.6475" height="30.9556" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-<feGaussianBlur stdDeviation="2.5" result="effect1_foregroundBlur_188_17175"/>
-</filter>
-<filter id="filter2_f_188_17175" x="195.915" y="313.507" width="40.585" height="8.99268" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-<feGaussianBlur stdDeviation="1.5" result="effect1_foregroundBlur_188_17175"/>
-</filter>
-<filter id="filter3_f_188_17175" x="195.915" y="280.5" width="40.585" height="8.99268" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-<feGaussianBlur stdDeviation="1.5" result="effect1_foregroundBlur_188_17175"/>
-</filter>
-<linearGradient id="paint0_linear_188_17175" x1="215.917" y1="299.827" x2="215.917" y2="282.447" gradientUnits="userSpaceOnUse">
-<stop stop-color="white"/>
-<stop offset="1" stop-color="white" stop-opacity="0"/>
-</linearGradient>
-</defs>
 </svg>
 `
