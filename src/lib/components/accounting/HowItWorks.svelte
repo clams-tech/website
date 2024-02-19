e846b25d9de9c0acdae77832e2d66911bf50ce59
johngribbin <30157175+johngribbin@users.noreply.github.com>
2024-02-19 11:49:03 -0500

    Add new icons

--- src/lib/components/accounting/HowItWorks.svelte
+++ src/lib/components/accounting/HowItWorks.svelte
@@ -8,18 +8,22 @@
 	const tiles = [
 		{
 			title: 'SYNC',
+			description: 'Import all transaction data to a local, private hub.',
 			icon: SyncIcon
 		},
 		{
 			title: 'ENHANCE',
+			description: 'Conversion of data for double-entry bookkeeping.',
 			icon: EnhanceIcon
 		},
 		{
 			title: 'VISUALIZE',
+			description: 'Derive meaning via sats flow dashboard.',
 			icon: VisualizeIcon
 		},
 		{
 			title: 'REPORT',
+			description: 'Download reports to share with third parties.',
 			icon: ReportIcon
 		}
 	]
@@ -27,15 +31,18 @@
 
 <div class="bg-white dark:bg-gray-900">
 	<div class="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
-		<div class="mx-auto  text-center">
-			<h2 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+		<div class="mx-auto max-w-4xl">
+			<h2
+				class="text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl"
+			>
 				How it works
 			</h2>
-			<div class="mt-16 grid grid-cols-2 md:grid-cols-4 gap-8">
-				{#each tiles as { title, icon }}
-					<div class="border rounded-3xl shadow-xl p-4">
+			<div class="mt-16 grid grid-cols-1 md:grid-cols-2 gap-8">
+				{#each tiles as { title, description, icon }}
+					<div class="w-full border rounded-lg shadow-xl p-4 m-auto flex flex-col gap-2">
 						<h3 class="inline font-semibold text-gray-900 dark:text-white">{title}</h3>
-						<div class="mt-8 sm:w-40 md:w-60 m-auto">
+						<p>{description}</p>
+						<div class="mt-4 m-auto sm:w-40 md:w-60 flex justify-center">
 							{@html icon}
 						</div>
 					</div>
