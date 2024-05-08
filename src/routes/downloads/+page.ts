c1d99bdbf14e4e7744cdde39b8c4927c389820e1
johngribbin <30157175+johngribbin@users.noreply.github.com>
2024-05-08 09:08:08 -0400

    Update api call to fetch release from new repo

--- src/routes/downloads/+page.ts
+++ src/routes/downloads/+page.ts
@@ -1,8 +1,8 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
-	// @TODO Fetch Clams Native App download links
-	const response = await fetch('https://api.github.com/repos/clams-tech/Remote/releases/latest')
+	// https://api.github.com/repos/clams-tech/Remote/releases/latest
+	const response = await fetch('https://api.github.com/repos/clams-tech/app-releases/releases')
 	const data = await response.json()
 
-	return data
+	return data[0] // @TODO change to "data" when api call for "latest" is working for the new repo
 }
