c61a425f95c46dcd98ab470d83842a49a06a43f2
johngribbin <30157175+johngribbin@users.noreply.github.com>
2024-05-21 16:55:55 -0400

    Fix discord link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -28,8 +28,7 @@ export const FEATURE_ICONS: {
 }
 
 export const REMOTE_APP_URL = 'https://remote.clams.tech/'
-// export const DOCS_URL = 'http://localhost:8000'
-export const DOCS_URL = 'http://docs.clams.tech'
-export const DISCORD_URL = '//https://discord.gg/eWfHuJZVaB'
+export const DOCS_URL = 'https://docs.clams.tech'
+export const DISCORD_URL = 'https://discord.gg/eWfHuJZVaB'
 export const GITHUB_URL = 'https://github.com/clams-tech'
 export const CRAB_NEBULA_URL = 'https://cdn.crabnebula.app/download/clams/clams/latest/platform'
