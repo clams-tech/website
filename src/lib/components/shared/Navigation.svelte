2e83990436dd643a17328ba2e62562bd87ed1d24
John Gribbin <30157175+johngribbin@users.noreply.github.com>
2024-05-30 07:26:14 -0400

    hide pricing (#13)

--- src/lib/components/shared/Navigation.svelte
+++ src/lib/components/shared/Navigation.svelte
@@ -49,11 +49,11 @@
 					>Features</span
 				>
 
-				<span
+				<!-- <span
 					use:scrollto={'#pricing'}
 					class="cursor-pointer text-sm font-semibold leading-6 text-gray-900 dark:text-white dark:text-white"
 					>Pricing</span
-				>
+				> -->
 
 				<span
 					use:scrollto={'#faq'}
@@ -112,12 +112,12 @@
 								class="cursor-pointer -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800"
 								>Features</a
 							>
-							<a
+							<!-- <a
 								on:click={() => (showMobileMenu = false)}
 								href="#pricing"
 								class="cursor-pointer -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800"
 								>Pricing</a
-							>
+							> -->
 							<a
 								on:click={() => (showMobileMenu = false)}
 								href="#faq"
