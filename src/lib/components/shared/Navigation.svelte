2d16ca6808b2682a051e1a7a32c8816cbeec997a
johngribbin <30157175+johngribbin@users.noreply.github.com>
2025-01-01 14:33:30 +0000

    Tweak nav to accommodate new conditionals

--- src/lib/components/shared/Navigation.svelte
+++ src/lib/components/shared/Navigation.svelte
@@ -56,12 +56,6 @@
 					class="cursor-pointer text-sm font-semibold leading-6 text-gray-900 dark:text-white dark:text-white"
 					>FAQ</span
 				>
-				<a href={BLOG_URL} target="_blank" rel="noopener noreferrer">
-					<span
-						class="cursor-pointer text-sm font-semibold leading-6 text-gray-900 dark:text-white dark:text-white"
-						>Blog</span
-					>
-				</a>
 			</div>
 		{/if}
 		<!-- Download route -->
@@ -79,6 +73,14 @@
 				>
 			</div>
 		{/if}
+		<div class="hidden lg:flex">
+			<a class="ml-12" href={BLOG_URL} target="_blank" rel="noopener noreferrer">
+				<span
+					class="cursor-pointer text-sm font-semibold leading-6 text-gray-900 dark:text-white dark:text-white"
+					>Blog</span
+				>
+			</a>
+		</div>
 		<div class="hidden lg:flex lg:flex-1 lg:justify-end">
 			<a
 				href={downloadHref}
@@ -162,6 +164,14 @@
 								href="/#faq"
 								class="cursor-pointer -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800"
 								>FAQ</a
+							>
+							<a
+								on:click={() => (showMobileMenu = false)}
+								href={BLOG_URL}
+								target="_blank"
+								rel="noopener noreferrer"
+								class="cursor-pointer -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800"
+								>Blog</a
 							>
 						</div>
 					{/if}
